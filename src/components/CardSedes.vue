de2108b404bdef6c799e1d7752c02e2fa7d29220
Ana Martinez <ana.martinez.mix@hotmail.com>
2023-09-24 17:07:14 -0600

    Modi

--- src/components/CardSedes.vue
+++ src/components/CardSedes.vue
@@ -1,71 +1,166 @@
 <script setup lang="ts">
 </script>
 
-<!-- <template>
-   <template> -->
-  <!-- Parte del modal 1 cuando se le da clic-->
-  <!-- <div class="portfolio-modal modal fade" :id="'portfolioModal1'" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="close-modal" @click="closeModal">
-          <img src="assets/imagenes/close-icon.svg" alt="Close modal" />
-        </div>
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-8">
-              <div class="modal-body">
-                Detalles de cuando se abre el modal
-                <CardSedes
-                  :modalId="1"
-                  modalTitle="El espacio revolucionando tu vida (2017)"
-                  sede="Chilpancingo de los Bravo, Gro."
-                  modalImage="assets/imagenes/Portfolio/2017.png"
-                  modalDescription="Esta es una imagen de los jóvenes voluntarios (divulgadores de ciencia) que participaron en este año!"
-                />
-                <button class="btn btn-primary btn-xl text-uppercase" @click="closeModal">
-                  <i class="fas fa-times me-1"></i> Cerrar
-                </button>
-              </div>
+<template>
+   <template>
+   <!-- Parte del modal 1 cuando se le da clic-->
+   <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="modal-body">
+                                <!-- Detalles de cuando se abre el modal-->
+                                <h3 class="text-uppercase">El espacio revolucionando tu vida(2017)</h3>
+                                <p class="item-intro text-muted">Sede: Chilpancingo de los Bravo, Gro.</p>
+                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/2017.png" alt="..." />
+                                <p class="item-intro text-muted">¡Está es una imagen de los jovenes voluntarios(divulgadores de ciencia) que participaron en este año!</p>
+                                <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                    <i class="fas fa-xmark me-1"></i>
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div> -->
-
-  <!-- Parte del modal 2 cuando se le da clic-->
-  <!-- <div class="portfolio-modal modal fade" :id="'portfolioModal2'" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="close-modal" data-bs-dismiss="modal"><img src="assets/imagen/close-icon.svg" alt="Close modal" /></div>
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-8">
-              <div class="modal-body">
-                Detalles de cuando se abre el modal
-                <h2 class="text-uppercase">Cosmovisiones, las historias del cielo(2018).</h2>
-                <p class="item-intro text-muted">Sede: Pilcaya, Gro.</p>
-                <img class="img-fluid d-block mx-auto" src="assets/imagen/Portfolio/2018.png" alt="..." />
-                <p>Observación nocturna mediante telescopio azimutal</p>
-                <button class="btn btn-primary btn-xl text-uppercase" @click="closeModal">
-                  <i class="fas fa-times me-1"></i> Cerrar
-                </button>
-              </div>
+    <!-- Parte del modal 2 cuando se le da clic-->
+    <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="modal-body">
+                                <!-- Detalles de cuando se abre el modal-->
+                                <h3 class="text-uppercase">Cosmovisiones, las historias del cielo(2018).</h3>
+                                <p class="item-intro text-muted">Sede: Pilcaya, Gro.</p>
+                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/2018.png" alt="..." />
+                                <p class="item-intro text-muted">Observación nocturna a cielo medio mediante telescopio azimutal</p>
+                                <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                    <i class="fas fa-xmark me-1"></i>
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div> -->
+        <!-- Parte del modal 3 cuando se le da clic-->
+    <div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="modal-body">
+                                <!-- Detalles de cuando se abre el modal-->
+                                <h3 class="text-uppercase">La tabla periodica (2019).</h3>
+                                <p class="item-intro text-muted">Sub-sede: Zitlala,Gro.</p>
+                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/2019.png" alt="..." />
+                                <p class="item-intro text-muted">Caminata en el desfile de faroles, realizada por las calles principales de la cuidad.</p>
+                                <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                    <i class="fas fa-xmark me-1"></i>
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Parte del modal 4 cuando se le da clic-->
+    <div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="modal-body">
+                                <!-- Detalles de cuando se abre el modal-->
+                                <h3 class="text-uppercase">El Universo develado (2020).</h3>
+                                <p class="item-intro text-muted">Sede: on-line</p>
+                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/2020.PNG" alt="..." />
+                                <p class="item-intro text-muted">En este año por desgracia no hubo sedes físicas y sólo se hicieron transmisiones por las redes sociales de todo el país con 70 sedes nacionales y 8 paises replicaron el contenido</p>
+                                <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                    <i class="fas fa-xmark me-1"></i>
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Parte del modal 5 cuando se le da clic-->
+    <div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="modal-body">
+                                <!-- Detalles de cuando se abre el modal-->
+                                <h3 class="text-uppercase">Armonía y Revolución (2021).</h3>
+                                <p class="item-intro text-muted">Sede: On-line.</p>
+                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/2021.jpg" alt="..." />
+                                <p class="item-intro text-muted">Nuevamente no hubo sedes físicas todo fue transmitido por redes sociales teniendo una audiencia de 32,358 personas</p>
+                                <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                    <i class="fas fa-xmark me-1"></i>
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Parte del modal 6 cuando se le da clic-->
+    <div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="modal-body">
+                                <!-- Detalles de cuando se abre el modal-->
+                                <h3 class="text-uppercase">La ciencia, sustento del desarrollo (2022).</h3>
+                                <p class="item-intro text-muted">Sede: Atoyac de álvarez, Gro.</p>
+                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/2022.JPG" alt="..." />
+                                <p class="item-intro text-muted">Divulgadores de ciencia, astronomos y miembros del H. Ayuntamiento del municipio en la sede.</p>
+                                <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                    <i class="fas fa-xmark me-1"></i>
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-  <!-- Agrega aquí los otros modales en el mismo formato -->
-<!-- </template>
+</template>
 
 <script>
 export default {
   methods: {
     closeModal() { -->
-      <!-- // Agrega aquí la lógica para cerrar el modal
+      <!-- 
     },
   },
 }; -->
