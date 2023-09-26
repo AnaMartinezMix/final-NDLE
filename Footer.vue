20f8c78186a41f163ee7bae477d18fbd490df0a9
JulisaJ10 <55122258+JulisaJ10@users.noreply.github.com>
2023-09-25 23:36:05 -0600

    configuracion footer

---
+++ Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <footer class="footer py-19 bg-trasparent text-Dark ">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-7 text-lg">
+            <b>Derechos Reservados &copy; NdE (Sede Chilpancingo 2023).</b>
+          </div>
+          <div class="col-lg-4 my-1 my-lg-0">
+            <a class="btn btn-outline-light btn-social mx-2" href="https://www.facebook.com/NdEChilpancingo"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-outline-light btn-social mx-2" href="mailto: NdLEstrellasC@gmail.com?cc=Hola, necesito más información&body=Quisiera%20m%C3%A1s%20informes%20"><i class="far fa-envelope"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+/* Personaliza el estilo del pie de página */
+.footer {
+  background-color: transparent; 
+  color: #00000036; 
+}
+
+.btn-social {
+  font-size: 10px; /* Ajusta el tamaño de los iconos de redes sociales */
+  
+}
+
+.btn-social {
+  font-size: 10;
+  background-color: rgba(72, 71, 71, 0.32); /* Cambia el color de los iconos de redes sociales */
+  border-radius:690%;
+  border-color: yellow;
+  width: 37px;
+  height: 37px;
+  line-height: 20px;
+  text-align: center;
+  transition: background-color 0.3s, color 0.3s; /* Configura la transición del color de los iconos de redes sociales */
+}
+</style>
