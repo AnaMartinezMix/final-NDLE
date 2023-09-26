fcd55f2428eb048205f7cf518d93a98f021caf56
JulisaJ10 <55122258+JulisaJ10@users.noreply.github.com>
2023-09-25 23:40:19 -0600

    color

---
+++ Header.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg fixed-top bc-">
+      <a class="navbar-brand">
+        <img src="@/assets/imagenes/Logo.png"  width="300" height="150" alt="Logo" class="logo-image">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto"> 
+          <li class="nav-item">
+            <a class="nav-link" href="inicio">INICIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="conocenos">CONÓCENOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="servicios">SERVICIOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sedes">SEDES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="nuestroequipo">NUESTRO EQUIPO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Suscribete">SUSCRÍBETE</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div> 
+</template>
+
+<style>
+.cont {
+  display: flex;
+  justify-content: flex-end;
+  padding: 100px;
+  background-color: transparent;
+  color: transparent;
+}
+
+.logo-image{
+  max-width: 190px;
+  height: fit-content;
+  margin-right: 400px;
+  padding-left: 1%;
+}
+
+.bc- {
+  background: rgba(106, 40, 206, 0.9);
+}
+
+/* Caracteristicas del menu de la cabecera. */
+.navbar-nav .nav-link {
+  color: rgb(255, 255, 255); 
+  font-size: 20px; 
+  margin-right: 20px;
+  margin-inline: 10px; 
+  text-transform: uppercase; 
+  transition: color 0.1s; 
+  font-family: 'Noto Sans';
+}
+/* Color de fondo de la cabecera. */
+
+.navbar-nav .nav-link:hover {
+  color: #c4412300;
+}
+</style>
+
+
+
